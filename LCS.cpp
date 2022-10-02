73da2b8747a81cd369ab2b8fe2404232ed455e8c
Prateek Rai <59793345+prateek11rai@users.noreply.github.com>
2022-10-02 14:32:13 +0530

    Add files via upload

---
+++ LCS.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
