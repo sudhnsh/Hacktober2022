68f7959a898b99fa4944b8bfb41bba45bcff0401
Rijul Sharma <67377115+rijulsharma7@users.noreply.github.com>
2022-10-03 21:07:52 +0530

    Add files via upload

---
+++ C++/PRIME_XOR.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vs = vector<string>;
+using ml = map<ll, ll>;
+using uml = unordered_map<ll, ll>;
+using pl = pair<ll, ll>;
+using tl = tuple<ll, ll, ll>;
+using vll = vector<pl>;
+using vlll = vector<tl>;
+using pq = priority_queue <ll, vl, greater<ll>>;
+using gq = priority_queue <ll>;
+#define mem(dp) memset(dp, -1, sizeof(dp))
+#define aut(a, b) for (auto&(a) : (b))
+#define out(v) for (auto&(x) : (v)) cout << x << " "; cout << '\n';
+#define rep(i, n) for (ll (i) = 0; (i) < (n); ++(i) )
+#define repp(i, n) for (ll (i) = 1; (i) <= (n); ++(i) )
+#define rev(i, n) for (ll (i) = n-1; (i) >= 0; --(i) )
+#define revv(i, n) for (ll (i) = n; (i) > 0; --(i) )
+#define all(v) v.begin(), v.end()
+#define fi get<0>
+#define se get<1>
+#define th get<2>
+#define F first
+#define S second
+#define mp 
+#define mt make_tuple
+#define pb push_back
+ll inf = 1e18; ll mod = 1e9 + 7;
+bool compare(ll a, ll b) {return a > b;}
+ll digit(ll n) {return floor(log10(n) + 1);}
+
+bool is(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i <= sqrt(n); i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+ 
+void calc()
+{
+    ll x, y, z, a, b, c; cin>>x>>y;
+    z=x^y;
+    if(x&1 and y&1) 
+    {
+        b=2, a=x^2, c=y^2;
+        cout<<b<<" "<<min(a, c)<<" "<<max(a, c)<<'\n';
+        return;
+    }
+    if(x&1) 
+    {
+        a=2, b=x^2, c=z^2;
+        cout<<a<<" "<<min(b, c)<<" "<<max(b, c)<<'\n';
+        return;
+    }
+    c=2, a=z^2, b=y^2;
+    cout<<c<<" "<<min(b, a)<<" "<<max(b, a)<<'\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    repp(z, t)
+    {
+        //cout << "Case #" << z << ": ";
+        calc();
+        // cout << '\n';
+    }
+
+    return 0;
+}
